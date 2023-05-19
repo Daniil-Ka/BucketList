50173fb635b0277a9bfa52a5353094fdbdfd5a06
ToyoTaSX <103138630+ToyoTaSX@users.noreply.github.com>
2023-05-19 07:14:35 +0500

    .

--- app/src/main/java/com/tms/bucketlist/ui/profile/LocalProfileService.kt
+++ app/src/main/java/com/tms/bucketlist/ui/profile/LocalProfileService.kt
@@ -1,6 +1,39 @@
 package com.tms.bucketlist.ui.profile
 
+import android.content.SharedPreferences
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import com.github.javafaker.Faker
+import com.github.javafaker.PhoneNumber
+import com.tms.bucketlist.domain.Category
+import com.tms.bucketlist.domain.Privacy
+import com.tms.bucketlist.domain.Target
+
 /** Хранит информацию о локальном пользователе и все его данные на устройстве */
-class LocalProfileService {
+class LocalProfileService private constructor() {
+    var name = ""
+    var age = ""
+    var sex = ""
+    var email = " "
+    var phoneNumber = ""
+    var photo = Drawable.createFromPath("res/drawable/circle.xml")
+    var data : SharedPreferences? = null
+
+    companion object {
+        val instance = LocalProfileService()
+    }
+
+    init {
+        //data = getSharedPreferences()
+    }
+
+    fun Update(name: String, age: String, sex: String, email: String, phoneNumber: String) {
+        this.name = name
+        this.age = age
+        this.sex = sex
+        this.email = email
+        this.phoneNumber = phoneNumber
+    }
 
 }
--- app/src/main/java/com/tms/bucketlist/ui/targets/details/TargetDetailsFragment.kt
+++ app/src/main/java/com/tms/bucketlist/ui/targets/details/TargetDetailsFragment.kt
@@ -1,32 +1,59 @@
 package com.tms.bucketlist.ui.targets.details
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.navArgs
 import com.tms.bucketlist.R
+import com.tms.bucketlist.TargetsRepository
 import com.tms.bucketlist.databinding.FragmentDetailsTargetBinding
 
-class TargetDetailsFragment : Fragment() {
+class TargetDetailsFragment : DialogFragment() {
 
     private val args: TargetDetailsFragmentArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val targetId = args.Id
-        Toast.makeText(context, targetId.toString(), Toast.LENGTH_LONG).show()
+
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentDetailsTargetBinding.bind(view)
+        dialog?.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
-        binding.root
+        val target = TargetsRepository.instance.getTargetById(args.Id)
+        if (target != null) {
+            val target_name = view?.findViewById<TextView>(R.id.target_name)
+            target_name?.text = target.name
+            // TODO ДОПИСАТЬ ВЫВОД ПОДЗАДАЧ СПИСКОМ, ВЫПОЛНЕННЫЕ ЗЕЛЕНЫМ
+            //             textView?.setTextColor(Color.GREEN)
+            // TODO Дедлайн для задачи и участники нужно добавить в класс цели
+            val target_descripton = view?.findViewById<TextView>(R.id.target_description)
+            target_descripton?.text = "Заметки: " + target.description
+        }
 
-        /*
+        // TODO НЕ РАБОТАЛ ВОЗВРАТ ЧЕРЕЗ НАВИГАЦИЮ (КОД ВСЕ ЕЩЕ ОСТАВЛСЯ ТАМ)
+        // TODO Оставил через прямое закрытие
+        val exitButton = view?.findViewById<ImageButton>(R.id.target_return_button)
+        exitButton?.setOnClickListener {
+            e -> dialog?.dismiss()
+        }
+            binding.root
+
+            /*
                 super.onViewCreated(view, savedInstanceState)
         val binding = FragmentCatDetailsBinding.bind(view)
         viewModel.catLiveData.observe(viewLifecycleOwner) { cat ->
@@ -51,15 +78,15 @@ class TargetDetailsFragment : Fragment() {
             viewModel.toggleFavorite()
         }
          */
-        //binding.goBackButton.setOnClickListener {
-        //    findNavController().popBackStack()
-        // }
-    }
+            //binding.goBackButton.setOnClickListener {
+            //    findNavController().popBackStack()
+            // }
+        }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_details_target, container, false)
-    }
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            return inflater.inflate(R.layout.fragment_details_target, container, false)
+        }
 }
