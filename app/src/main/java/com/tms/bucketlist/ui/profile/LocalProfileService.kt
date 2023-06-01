eb593ab1afc9aba5bd77a4995b5ccff88e9f9e45
ToyoTaSX <103138630+ToyoTaSX@users.noreply.github.com>
2023-06-01 10:05:34 +0500

    Хранение данных пользователя

--- app/src/main/java/com/tms/bucketlist/ui/profile/LocalProfileService.kt
+++ app/src/main/java/com/tms/bucketlist/ui/profile/LocalProfileService.kt
@@ -1,6 +1,7 @@
 package com.tms.bucketlist.ui.profile
 
-import android.content.SharedPreferences
+
+/**import android.content.SharedPreferences
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import androidx.core.content.ContextCompat
@@ -9,33 +10,63 @@ import com.github.javafaker.PhoneNumber
 import com.tms.bucketlist.domain.Category
 import com.tms.bucketlist.domain.Privacy
 import com.tms.bucketlist.domain.Target
+*/
 
 /** Хранит информацию о локальном пользователе и все его данные на устройстве */
-class LocalProfileService private constructor(
-    var name: String,
+/**var name: String,
     var age: String,
     var sex: String,
     var email: String,
     var phoneNumber: String,
     var photo: Drawable?,
     var data: SharedPreferences?,
-) {
+*/
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.graphics.drawable.Drawable
+
+
+class ProfileData private constructor(context: Context){
+    private val sharedPreferences: SharedPreferences = context.getSharedPreferences("profileData", Context.MODE_PRIVATE)
 
     companion object {
-        val instance = LocalProfileService("", "", "", "", "", null, null)
+        private var instance: ProfileData? = null
+
+        fun getInstance(context: Context): ProfileData {
+            return instance ?: synchronized(this) {
+                instance ?: ProfileData(context).also { instance = it }
+            }
+        }
     }
 
-    init {
-        //data = getSharedPreferences()
-    }
+    var name: String
+        get() = sharedPreferences.getString("name", "") ?: ""
+        set(value) {
+            sharedPreferences.edit().putString("name", value).apply()
+        }
 
-    fun Update(name: String, age: String, sex: String, email: String, phoneNumber: String) {
-        this.name = name
-        this.age = age
-        this.sex = sex
-        this.email = email
-        this.phoneNumber = phoneNumber
-        photo = Drawable.createFromPath("res/drawable/circle.xml")
-    }
+    var age: Int
+        get() = sharedPreferences.getInt("name", 0)
+        set(value) {
+            sharedPreferences.edit().putInt("name", value).apply()
+        }
 
+    var sex: String
+        get() = sharedPreferences.getString("sex", "") ?: ""
+        set(value) {
+            sharedPreferences.edit().putString("sex", value).apply()
+        }
+
+    var enail: String
+        get() = sharedPreferences.getString("email", "") ?: ""
+        set(value) {
+            sharedPreferences.edit().putString("email", value).apply()
+        }
+
+    var phoneNumber: String
+        get() = sharedPreferences.getString("phoneNumber", "") ?: ""
+        set(value) {
+            sharedPreferences.edit().putString("phoneNumber", value).apply()
+        }
 }
